8afcac639f9be8783fb49072db9569040d350e0d
陈思聪 <hannibal@chensicongdeMacBook-Pro.local>
2018-11-26 15:24:37 +0800

    CDN拉流示例

---
+++ docs/cdn/cdn.js
@@ -0,0 +1,78 @@
+var videoElement = document.getElementById('test');
+
+  $('#openRoom').click(function(){
+    openRoom($('#roomId').val(), 2)
+    videoElement.play()             //解决移动端无法自动播放
+  })
+  
+function init() {
+
+  zg = new ZegoClient();
+
+  //测试用代码，客户请忽略  start
+    if (location.search) {
+      let _arr_config = location.search.substr(1).split('&');
+      _arr_config.forEach(function (item)  {
+          var key = item.split('=')[0], value = item.split('=')[1];
+          if (value && _config.hasOwnProperty(key)) {
+              _config[key] = decodeURIComponent(value);
+          } else if (value && _otherConfig.hasOwnProperty(key)) {
+              _otherConfig[key] = decodeURIComponent(value);
+          }
+    });
+  }
+  //测试用代码，客户请忽略  end
+
+  console.log("config param:" + JSON.stringify(_config));
+  _config.appid = 1739272706;
+  zg.config(_config);
+  //测试用代码，客户请忽略  start
+  if(_otherConfig.signal){
+      zg.setCustomSignalUrl(_otherConfig.signal);
+  }
+  //测试用代码，客户请忽略  end
+  enumDevices();
+}
+
+//覆盖common.js中的loginSuccess
+function loginSuccess(streamList, type) {
+  var maxNumber = ($('#maxPullNamber') && $('#maxPullNamber').val()) || 4
+ 
+  //限制房间最多人数，原因：视频软解码消耗cpu，浏览器之间能支撑的个数会有差异，太多会卡顿
+  if (streamList.length >= maxNumber) {
+    alert('房间太拥挤，换一个吧！');
+    leaveRoom();
+    return;
+  }
+
+  if(type == 2){
+     //获取当前浏览器类型
+    var browser = getBrowser ();
+
+    if(browser == "Safari"){
+      videoElement.src = streamList[0].urls_hls[0]
+    }else if(streamList.length !== 0){
+      console.log(streamList)
+      var flvUrl = streamList[0].urls_https_flv[0] ;
+      //若支持flv.js
+      if (flvjs.isSupported()) {
+      var flvPlayer = flvjs.createPlayer({
+        type: 'flv',
+        url: flvUrl
+      });
+      flvPlayer.attachMediaElement(videoElement);
+      flvPlayer.load();
+      }
+    }else {
+      alert("未找到流");
+    }
+  }
+  
+  console.log(`login success`);
+  loginRoom = true;
+  // 监听sdk回掉
+  listen();
+
+  //开始预览本地视频
+  type === 1 && doPreviewPublish();
+}
