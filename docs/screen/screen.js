ae8a744c5369bc7a510866bac2bda502d3b090ed
陈思聪 <hannibal@chensicongdeMacBook-Pro.local>
2019-01-25 16:25:11 +0800

    fix firefox screenshare

--- docs/screen/screen.js
+++ docs/screen/screen.js
@@ -20,6 +20,7 @@ $(function () {
                 console.log('startScreenShot:'+suc);
                 screenCaptrue = suc;
                 previewVideo.srcObject = mediastream;
+                config.externalMediaStream = mediastream;
                 // 推送屏幕可有两种形式，一是作为流媒体直接推送 即下面这种形式
                 //另一种是作为externalCapture，前提是需要先将流喂给video标签；，下面chrome推送方式就是这种形式；可任意选择其中之一
                 if(loginRoom) {
