b727485d12b3c179f80bbe405666311cc28c6a09
陈思聪 <hannibal@chensicongdeMacBook-Pro.local>
2019-04-30 14:55:43 +0800

    增加无插件屏幕共享

--- docs/screen/screen.js
+++ docs/screen/screen.js
@@ -1,56 +1,76 @@
 
 
 $(function () {
+
+    function scrennShot(bool) {
+
+      if(IsPC()){
+
+          loginRoom && zg.stopPublishingStream(_config.idName);
+          loginRoom && zg.stopPreview(previewVideo);
+
+          var config = {
+              externalMediaStream:null,
+              width:640,
+              height:480,
+              frameRate:15,
+              bitRate:1000
+          };
+
+          getBrowser() === 'Firefox' && zg.startScreenShotFirFox('window',true,function (suc,mediastream) {
+              console.log('startScreenShot:'+suc);
+              screenCaptrue = suc;
+              previewVideo.srcObject = mediastream;
+              config.externalMediaStream = mediastream;
+              // 推送屏幕可有两种形式，一是作为流媒体直接推送 即下面这种形式
+              //另一种是作为externalCapture，前提是需要先将流喂给video标签；，下面chrome推送方式就是这种形式；可任意选择其中之一
+              if(loginRoom) {
+                  doPreviewPublish(config);
+              }
+          });
+
+          getBrowser() === 'Chrome' && bool &&zg.startScreenShotChrome(function (suc,mediastream) {
+              console.log('startScreenShot:'+suc);
+              screenCaptrue = suc;
+              // 推送屏幕可有两种形式，一是作为externalCapture，前提是需要先将流喂给video标签；即下面这种形式
+              //另一种是作为流媒体直接推送，上面火狐推送方式就是这种形式；可任意选择其中之一
+              previewVideo.srcObject = mediastream;
+              if(loginRoom) {
+                  doPreviewPublish({externalCapture:true});
+              }
+          })
+
+          getBrowser() === 'Chrome' && !bool && zg.startScreenSharingChrome(false, function (suc,mediastream) {
+            console.log('startScreenShot:'+suc);
+            screenCaptrue = suc;
+            // 推送屏幕可有两种形式，一是作为externalCapture，前提是需要先将流喂给video标签；即下面这种形式
+            //另一种是作为流媒体直接推送，上面火狐推送方式就是这种形式；可任意选择其中之一
+            previewVideo.srcObject = mediastream;
+            if(loginRoom) {
+                doPreviewPublish({externalCapture:true});
+            }
+          })
+      }
+    }
+
     $('#screenShot').click(function () {
-
-        if(IsPC()){
-
-            loginRoom && zg.stopPublishingStream(_config.idName);
-            loginRoom && zg.stopPreview(previewVideo);
-
-            var config = {
-                externalMediaStream:null,
-                width:640,
-                height:480,
-                frameRate:15,
-                bitRate:1000
-            };
-
-            getBrowser() === 'Firefox' && zg.startScreenShotFirFox('window',false,function (suc,mediastream) {
-                console.log('startScreenShot:'+suc);
-                screenCaptrue = suc;
-                previewVideo.srcObject = mediastream;
-                config.externalMediaStream = mediastream;
-                // 推送屏幕可有两种形式，一是作为流媒体直接推送 即下面这种形式
-                //另一种是作为externalCapture，前提是需要先将流喂给video标签；，下面chrome推送方式就是这种形式；可任意选择其中之一
-                if(loginRoom) {
-                    doPreviewPublish(config);
-                }
-            });
-
-            getBrowser() === 'Chrome' && zg.startScreenShotChrome(function (suc,mediastream) {
-                console.log('startScreenShot:'+suc);
-                screenCaptrue = suc;
-                // 推送屏幕可有两种形式，一是作为externalCapture，前提是需要先将流喂给video标签；即下面这种形式
-                //另一种是作为流媒体直接推送，上面火狐推送方式就是这种形式；可任意选择其中之一
-                previewVideo.srcObject = mediastream;
-                if(loginRoom) {
-                    doPreviewPublish({externalCapture:true});
-                }
-            })
-        }
+      scrennShot(true)
     });
+
+    $('#screenSharing').click(function () {
+      scrennShot(false)
+    })
 
     $('#stopScreenShot').click(function () {
         zg.stopScreenShot()
         zg.stopPreview(previewVideo);
         zg.stopPublishingStream(_config.idName);
-        
+
         doPreviewPublish();
     });
 
     $('#pushTwoStreams').click(function ()  {
-      
+
       if(!loginRoom) {
         alert('请先登录房间')
         return
@@ -68,7 +88,7 @@ $(function () {
           "externalCapture": false,
           "externalMediaStream": null
         }
-        
+
         idName = 'zego-'+ new Date().getTime()
 
         var result = zg.startPreview($('#previewVideo2')[0],previewConfig,function () {
@@ -87,7 +107,7 @@ $(function () {
         alert('请先开启屏幕共享')
       }
 
-     
+
 
     });
 
