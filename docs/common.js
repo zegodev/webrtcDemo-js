113229f35571bdbc40f5e402af9ac9b2c5b2238d
zhaowei <zhaowei@zego.im>
2019-07-30 18:17:18 +0800

    混流转码

--- docs/common.js
+++ docs/common.js
@@ -32,7 +32,7 @@ $(function () {
         ZegoClient.isSupportH264(result => {
             bindEvent();
             if (!result) {
-                alert('浏览器不支持视频h264编码，不能推拉视频流');
+                alert('浏览器不支持视频h264编码，建议使用vp8尝试');
             }
         }, err => {
             console.error(err);
